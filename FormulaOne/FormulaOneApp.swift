2ac73dbacd9389e24ddf22fc20be3921f57fe366
Anthony DC <me@anthony-dacruz.com>
2024-02-15 16:05:08 +0100

    Initial Commit

---
+++ FormulaOne/FormulaOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaOneApp.swift
+//  FormulaOne
+//
+//  Created by Anthony Da Cruz on 15/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct FormulaOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
